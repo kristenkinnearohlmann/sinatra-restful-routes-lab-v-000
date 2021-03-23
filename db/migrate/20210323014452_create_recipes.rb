3c5d9c727c8099d8cf7a4ee9c7184d1892e2c396
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-23 01:45:32 +0000

    Automatically backed up by Learn

--- db/migrate/20210323014452_create_recipes.rb
+++ db/migrate/20210323014452_create_recipes.rb
@@ -1,4 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
+    creat_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.integer :cook_time
+    end
   end
 end
